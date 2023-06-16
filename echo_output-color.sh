4f99a3564a6e4854db7e1cbf96d0991a126ed7d2
saiful-dev <saiful.hstu.sii@gmail.com>
2023-06-16 11:33:54 +0600

    echo output color format changes

---
+++ echo_output-color.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+echo -e "I ${RED}love${NOCOLOR} Linux"
+
+#The optional -e flag of the echo command allows you to use special characters like \n (newline) and \t (tab) inside of the input string
